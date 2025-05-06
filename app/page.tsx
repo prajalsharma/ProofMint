d9397734854b4e2f8cc0747bb05ced87e3b3318b
Aditya Phasu <adityaphasu919@gmail.com>
2025-05-07 00:02:59 +0530

    update and improve layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
           <CardContent className="px-6 flex flex-col gap-4 items-start justify-start text-left">
             <div className="flex items-center justify-between gap-2 w-full">
               <h2 className="text-3xl font-semibold text-brownish">Anonymous Feedback</h2>
-              <Image src="/arrow.svg" alt="arrow" width={95} height={95} />
+              <Image src="/arrow.svg" alt="" width={95} height={95} />
             </div>
             <p className="text-cream text-sm">
               Prove organizational affiliation or log in with your work email to submit feedback.
@@ -32,9 +32,12 @@ export default function Page() {
         </Card>
 
         <Card className="bg-[#18132f] border border-[#2c254b]">
-          <CardContent className="px-6 flex flex-col gap-4 items-start justify-start text-left">
-            <h2 className="text-3xl font-semibold text-brownish">Polling</h2>
-            <p className="text-cream text-sm">
+          <CardContent className="px-6 flex flex-col gap-4 items-start justify-between text-left h-full">
+            <div className="flex items-center justify-between gap-2 w-full">
+              <h2 className="text-3xl font-semibold text-brownish">Polling</h2>
+              <Image src="/graph.svg" alt="" width={75} height={75} />
+            </div>
+            <p className="text-cream text-sm mt-3.5">
               Log in with ZK or your email to participate in a poll, then anonymously view results.
             </p>
             <Button className="text-cream border-2 border-cream bg-transparent font-medium hover:text-darkblue hover:bg-cream w-full">
