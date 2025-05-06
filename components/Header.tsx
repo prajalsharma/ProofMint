480b5702b473f682f4896f6e3a0ef3af477603f4
Aditya Phasu <adityaphasu919@gmail.com>
2025-05-07 00:02:21 +0530

    add Header, Button, Card components and utility function for class name merging

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Wallet } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="w-full max-w-4xl flex justify-between items-center mb-8">
+      <div className="flex items-center gap-2 text-xl font-semibold">
+        <a href="/" className="flex items-center gap-2 uppercase text-2xl">
+          <Image src="/logo.svg" alt="Logo" width={75} height={75} className="" />
+          <span className="text-cream">
+            Proof<span className="text-brownish">Mint</span>
+          </span>
+        </a>
+      </div>
+      <Button className="text-cream text-lg border-1 border-cream bg-transparent hover:bg-cream hover:text-darkblue transition-colors px-10 py-5">
+        <Wallet className="mr-1" /> <span className="hidden md:block">Connect Wallet</span>
+      </Button>
+    </header>
+  );
+};
+export default Header;
